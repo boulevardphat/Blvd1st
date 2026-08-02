1bd71ac9e79425f4b00c179ba5ff63619f92a0c6
chatthuanpho <118000192+boulevardphat@users.noreply.github.com>
2026-08-02 10:51:49 +0700

    refactor: remove legacy migration scripts
    Delete outdated Python automation scripts used for project refactoring and content updates.

--- test2.py
+++
@@ -1,9 +0,0 @@
-import sys
-content = open('src/App.tsx').read()
-
-import re
-# Remove the LandscapeContactMenu component definition
-content = re.sub(r'// --- Landscape Contact Menu ---.*?const LandscapeContactMenu = \(\) => \{.*?(?=// --- Landscape Contact Screen Component)', '', content, flags=re.DOTALL)
-
-with open('src/App.tsx', 'w') as f:
-    f.write(content)
